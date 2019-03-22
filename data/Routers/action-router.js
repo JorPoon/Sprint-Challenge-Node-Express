5b5fb56d4bc22cf6b8eeb49b2f6a5bd86d11abf5
Jor_Poon <jormpoon@gmail.com>
2019-03-22 12:11:05 -0400

    can get actions by id

--- data/Routers/action-router.js
+++ data/Routers/action-router.js
@@ -6,12 +6,26 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const actions = await Actions.get(req)
+        const actions = await Actions.get()
         res.status(200).json(actions)
     } catch (error) {
         res.status(500).json({error: 'Error retrieving actions'})
     }
 });
+
+router.get('/:id', async (req, res) => {
+   try {
+       const actionId = await Actions.get(req.params.id);
+       if (actionId) {
+           res.status(200).json(actionId)
+       } else {
+           res.status(404).json({error: 'Action cannot be found'});
+       }
+   } catch (error) {
+       console.log(error);
+       res.status(500).json({error: 'Error retrieving action'})
+   }
+})
 
 
 module.exports = router;
