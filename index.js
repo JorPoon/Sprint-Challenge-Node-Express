9eed7f20ca59e9011d78ae8ac3d3fe1720bb6408
Jor_Poon <jormpoon@gmail.com>
2019-03-22 11:40:28 -0400

    set up index.js file with server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 // code away!
+require('dotenv').config();
+const server = require('./server.js');
+
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
+
+server.listen(port, () => {
+    console.log(`\n*** ${greeting} on http://localhost:${port} ***\n`)
+})
